764afb039bb22092ffb75a91250ea8e0e08e770f
Daisuke Yokogawa <yokogawa@c-grip.com>
2017-01-06 10:56:40 +0900

    デバッグに$_SERVER追加

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -30,7 +30,12 @@ class SearchController extends AppController
 		// App::import('Sanitize');
 		
 		$query = $this->request->query;
-		$this->log($query, LOG_DEBUG);
+
+		$this->log(array(
+			"Server" => $_SERVER,
+			"Query" => $query,
+		), LOG_DEBUG);
+
 		if(empty($query["page"]))
 		{
 			$query["page"] = 1;
