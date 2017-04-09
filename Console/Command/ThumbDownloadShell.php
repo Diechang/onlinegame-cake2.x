340d75096e1786a995de9c1e1b82c3630422445e
Daisuke Yokogawa <diechan.dai.1031@gmail.com>
2017-04-09 23:49:18 +0900

    サムネダウンロードコマンド

---
+++ Console/Command/ThumbDownloadShell.php
@@ -0,0 +1,8 @@
+<?php
+class ThumbDownloadShell extends AppShell {
+
+    function main()
+    {
+    	// scp -r diechang@diechang.sakura.ne.jp:/home/diechang/www/onlinegame.dz-life.net/img/thumb ~/MAMP/htdocs/public_html/onlinegame.dz-life.net/img/
+    }
+}
